78812e73357464222e094cc7e8c5edc0c8aecdf1
Akbar014 <akbar.cse47@gmail.com>
2025-02-14 22:57:38 +0600

    from url scrape data and save in htnl file

--- Day 12/scrape.py
+++ Day 12/scrape.py
@@ -1,7 +1,24 @@
+import datetime
 import requests
 
-url ="https://www.boxofficemojo.com/year/world/"
 
-r = requests.get(url)
-print(r)
-print(r.status_code)
+now = datetime.datetime.now()
+year = now.yea
+
+
+
+
+def url_to_file(url, filename='world.html'):
+    r = requests.get(url)
+    if r.status_code == 200:
+        html_txt = r.text
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(html_txt)
+        return html_txt
+
+url ="https://www.boxofficemojo.com/year/world/"
+url_to_file(url)
+
+# r = requests.get(url)
+# print(r.text)
+# print(r.status_code)
